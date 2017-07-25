269511881a43a3b78ca192a2a694f4869766db0d
yashpatel5400 <ypatel@princeton.edu>
2017-07-24 23:45:05 -0700

    Added OpenAI Keras basic implementation

--- game.py
+++ game.py
@@ -58,27 +58,78 @@ print(times)
 
 # deep learning version
 
+import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 from keras.utils import np_utils
 import gym
+from keras.models import load_model
 
 env = gym.make("CartPole-v0")
-observation = env.reset()
+
+min_score = 50
+sim_steps = 500
+trainingX, trainingY = [], []
+
+# get data
+
+scores = []
+for _ in range(10000):
+	observation = env.reset()
+	score = 0
+	training_sampleX, training_sampleY = [], []
+	for step in range(sim_steps):
+		action = np.random.randint(0, 2)
+		# action corresponds to the previous observation so record before step
+		one_hot_action = np.zeros(2)
+		one_hot_action[action] = 1
+		training_sampleX.append(observation)
+		training_sampleY.append(one_hot_action)
+		
+		observation, reward, done, _ = env.step(action)
+		score += reward
+		if done:
+			break
+	if score > min_score:
+		scores.append(score)
+		trainingX += training_sampleX
+		trainingY += training_sampleY
+
+trainingX, trainingY = np.array(trainingX), np.array(trainingY)
+print("Average: {}".format(np.mean(scores)))
+print("Median: {}".format(np.median(scores)))
+
+# define model
 
 model = Sequential()
-model.add(Dense(2, input_shape=(4)))
-model.add(Dense(1, activation="softmax"))
+model.add(Dense(128, input_shape=(4,), activation="relu"))
+model.add(Dropout(0.6))
+
+model.add(Dense(256, activation="relu"))
+model.add(Dropout(0.6))
+
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.6))
+model.add(Dense(2, activation="softmax"))
+
 model.compile(
-	loss="mean_squared_error",
-	optimizer="adam")
+	loss="categorical_crossentropy",
+	optimizer="adam",
+	metrics=["accuracy"])
 
-value_model = Sequential()
-value_model.add(Dense(10, input_shape=(4)))
-value_model.add(Dense(1))
-value_model.compile(
-	loss="mean_squared_error",
-	optimizer="adam")
+# train model
+model.fit(trainingX, trainingY, epochs=5)
+scores = []
+num_trials = 50
+for trial in range(num_trials):
+	observation = env.reset()
+	score = 0
+	for step in range(sim_steps):
+		action = np.argmax(model.predict(observation.reshape(1,4)))
+		observation, reward, done, _ = env.step(action)
+		score += reward
+		if done:
+			break
+	scores.append(score)
 
-for _ in xrange(1000):
-	pass
+print(np.mean(scores))
