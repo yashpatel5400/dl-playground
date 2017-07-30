93872004c010131cda11f3e336a8117bdba4ee44
yashpatel5400 <ypatel@princeton.edu>
2017-07-30 15:29:34 -0700

    Running version completed

--- actor_critic.py
+++ actor_critic.py
@@ -18,7 +18,7 @@ from collections import deque
 # determines how to assign values to each state, i.e. takes the state
 # and action (two-input model) and determines the corresponding value
 class ActorCritic:
-	def __init__(self, sess, env):
+	def __init__(self, env, sess):
 		self.env  = env
 		self.sess = sess
 
@@ -30,14 +30,23 @@ class ActorCritic:
 
 		# ===================================================================== #
 		#                               Actor Model                             #
+		# Chain rule: find the gradient of chaging the actor network params in  #
+		# getting closest to the final value network predictions, i.e. de/dA    #
+		# Calculate de/dA as = de/dC * dC/dA, where e is error, C critic, A act #
 		# ===================================================================== #
 
 		self.memory = deque(maxlen=2000)
 		self.actor_state_input, self.actor_model = self.create_actor_model()
 		_, self.target_actor_model = self.create_actor_model()
 
-		self.critic_gradient = tf.placeholder(tf.float32,self.env.action_space.shape)
-		self.actor_grads = tf.gradients(self.actor_model.output, self.actor_action_input)
+		self.actor_critic_grad = tf.placeholder(tf.float32, 
+			[None, self.env.action_space.shape[0]]) # where we will feed de/dC (from critic)
+		
+		actor_model_weights = self.actor_model.trainable_weights
+		self.actor_grads = tf.gradients(self.actor_model.output, 
+			actor_model_weights, -self.actor_critic_grad) # dC/dA (from actor)
+		grads = zip(self.actor_grads, actor_model_weights)
+		self.optimize = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(grads)
 
 		# ===================================================================== #
 		#                              Critic Model                             #
@@ -47,10 +56,15 @@ class ActorCritic:
 			self.critic_model = self.create_critic_model()
 		_, _, self.target_critic_model = self.create_critic_model()
 
-		self.critic_grads = tf.gradients(self.critic_model.output, self.critic_action_input)
+		self.critic_grads = tf.gradients(self.critic_model.output, 
+			self.critic_action_input) # where we calcaulte de/dC for feeding above
 		
 		# Initialize for later gradient calculations
 		self.sess.run(tf.initialize_all_variables())
+
+	# ========================================================================= #
+	#                              Model Definitions                            #
+	# ========================================================================= #
 
 	def create_actor_model(self):
 		state_input = Input(shape=self.env.observation_space.shape)
@@ -81,22 +95,27 @@ class ActorCritic:
 		model.compile(loss="mse", optimizer=adam)
 		return state_input, action_input, model
 
+	# ========================================================================= #
+	#                               Model Training                              #
+	# ========================================================================= #
+
 	def remember(self, cur_state, action, reward, new_state, done):
 		self.memory.append([cur_state, action, reward, new_state, done])
 
-	def act(self, cur_state):
-		self.epsilon *= self.epsilon_decay
-		if np.random.random() < self.epsilon:
-			return self.env.action_space.sample()
-		return self.actor_model.predict(cur_state)
-
 	def _train_actor(self, samples):
 		for sample in samples:
-			grads = critic_grad * actor_grad
-			weights = self.actor_model.get_weights()
-			updated_weights = weights + self.learning_rate * grads
-			self.actor_model.set_weights(updated_weights)
-			
+			cur_state, action, reward, new_state, _ = sample
+			predicted_action = self.actor_model.predict(cur_state)
+			grads = self.sess.run(self.critic_grads, feed_dict={
+				self.critic_state_input:  cur_state,
+				self.critic_action_input: predicted_action
+			})[0]
+
+			self.sess.run(self.optimize, feed_dict={
+				self.actor_state_input: cur_state,
+				self.actor_critic_grad: grads
+			})
+            
 	def _train_critic(self, samples):
 		for sample in samples:
 			cur_state, action, reward, new_state, done = sample
@@ -114,8 +133,12 @@ class ActorCritic:
 
 		rewards = []
 		samples = random.sample(self.memory, batch_size)
-		self._train_actor(samples)
 		self._train_critic(samples)
+		self._train_actor(samples)
+
+	# ========================================================================= #
+	#                         Target Model Updating                             #
+	# ========================================================================= #
 
 	def _update_actor_target(self):
 		actor_model_weights  = self.actor_model.get_weights()
@@ -137,7 +160,17 @@ class ActorCritic:
 		self._update_actor_target()
 		self._update_critic_target()
 
-sess = tf.Session(config=config)
+	# ========================================================================= #
+	#                              Model Predictions                            #
+	# ========================================================================= #
+
+	def act(self, cur_state):
+		self.epsilon *= self.epsilon_decay
+		if np.random.random() < self.epsilon:
+			return self.env.action_space.sample()
+		return self.actor_model.predict(cur_state)
+
+sess = tf.Session()
 K.set_session(sess)
 env = gym.make("Pendulum-v0")
 actor_critic = ActorCritic(env, sess)
@@ -161,6 +194,3 @@ for _ in range(num_trials):
 		actor_critic.train()
 
 		cur_state = new_state
-		if done:
-			break
-	print("Ended in: {} steps".format(step))
